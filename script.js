6461939eb6ca3d309a63685977ddfb6b540e549c
Alexandre D. Gomes <alexandredanielgomes524@gmail.com>
2025-04-07 13:55:32 +0100

    Atualizei o projeto

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ let tenis = document.querySelector(".imagem-tenis");
 // querySelector = selecionar / pegar o elemento
 
 function mudarVisual(cor, imagem) {
+    tenis.classList.add("troca-efeito")
     
     body.style.background = cor
+
+    setTimeout(() => {
     tenis.src = imagem
+    tenis.classList.remove("troca-efeito")
+    }, 500);
+
 }
